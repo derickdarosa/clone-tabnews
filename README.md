cfeda77b9efbaf1d7b4f076b72daa95cae754bc3
Derick Rosa <90993862+derickdarosa@users.noreply.github.com>
2025-01-14 22:52:38 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para meus treinos de programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>É assim que começa uma nova história! Trilhando um caminho diário, com novos desafios. </h1>
+  return (
+    <h1>
+      É assim que começa uma nova história! Trilhando um caminho diário, com
+      novos desafios.{" "}
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentacao errada");
+}
 export default Home;
